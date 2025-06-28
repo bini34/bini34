59e73376933689fc31cdee05f3e4c5a542fcad04
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-06-28 12:21:26 +0300

    Add Skills component to display brand logos using InfiniteMovingCards

---
+++ src/components/skills.tsx
@@ -0,0 +1,27 @@
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards"
+
+function Skills() {
+    const brandLogos = [
+        { src: '/images/logos/logo.png', alt: 'Brand Logo 1' },
+        { src: '/images/logos/logo.png', alt: 'Brand Logo 2' },
+        { src: '/images/logos/logo.png', alt: 'Brand Logo 3' },
+        { src: '/images/logos/logo.png', alt: 'Brand Logo 4' },
+        { src: '/images/logos/logo.png', alt: 'Brand Logo 5' },
+      ]
+    
+      return (
+      
+        <div className="h-[5rem] rounded-md flex flex-col antialiased bg-transparent items-center justify-center w-full overflow-hidden p-0 ">
+            <InfiniteMovingCards
+                items={brandLogos}
+                direction="right"
+                speed="slow"
+            />
+        </div>
+    
+          
+      )
+  
+}
+
+export default Skills
