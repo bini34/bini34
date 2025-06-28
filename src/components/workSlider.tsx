c7e24639f646e993a8e117397beed35a998d1637
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-06-28 12:21:17 +0300

    Add WorkSlider component to display brand logos with InfiniteMovingCards

---
+++ src/components/workSlider.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { InfiniteMovingCards } from './ui/infinite-moving-cards'
+
+function WorkSlider() {
+    const brandLogos = [
+        { src: '/images/mlO56hcZ2tRpiredp0Ybs7avJk.avif', alt: 'Brand Logo 1' },
+        { src: '/images/mlO56hcZ2tRpiredp0Ybs7avJk.avif', alt: 'Brand Logo 2' },
+        { src: '/images/mlO56hcZ2tRpiredp0Ybs7avJk.avif', alt: 'Brand Logo 3' },
+        { src: '/images/mlO56hcZ2tRpiredp0Ybs7avJk.avif', alt: 'Brand Logo 4' },
+        { src: '/images/mlO56hcZ2tRpiredp0Ybs7avJk.avif', alt: 'Brand Logo 5' },
+      ]
+    
+      return (
+        <section>
+            <div className='w-full pl-16 font-bold text-9xl'>
+                <h1>MY WORK</h1>
+            </div>
+            <div className="h-[30rem] rounded-md flex flex-col antialiased bg-transparent items-center justify-center w-full overflow-hidden p-0 ">
+                <InfiniteMovingCards
+                    items={brandLogos}
+                    direction="right"
+                    speed="slow"
+                    height = '20rem'
+                />
+            </div>
+       </section>
+  )
+}
+
+export default WorkSlider
