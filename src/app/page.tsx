022e531f7820d95cab332575eacb29eb2e7806b0
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-06-28 17:51:58 +0300

    Add About and Contact components to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import Header from "@/components/header";
+import About from "@/components/about";
+import Contact from "@/components/contact";
 import Hero from "@/components/hero";
 import WorkSlider from "@/components/workSlider";
 
 export default function Home() {
   return (
-    <>
-   <Header/>
-   <main className=" flex flex-col gap-20">
-    <Hero/>
-    <WorkSlider/>
-   </main>
-    </>
+      <main className=" flex flex-col gap-20">
+        <Hero/>
+        <WorkSlider/>
+        <About/>
+        <Contact/>
+      </main>
     
   );
 }
