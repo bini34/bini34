f3fb48d9ebfbffbd00fdc6539eb915072bea798f
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-06-29 21:23:46 +0300

    Update Contact component layout and heading for improved responsiveness and clarity

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 function Contact() {
   return (
     <section className="flex  items-center  p-20">
-      <div className="w-[50%] flex flex-col gap-5">
-      `<h1 className="text-7xl font-bold">Let&apos;s Talk</h1>
+      <div className="w-full lg:w-[50%] flex flex-col gap-20 justify-center items-center lg:items-start">
+      `<h1 className="text-7xl font-bold">Get in Touch</h1>
   
         <div className="flex flex-col gap-3">
             <button className="w-[200px] border-3 border-gray-500 py-4 rounded-2xl">
@@ -15,7 +15,7 @@ function Contact() {
         </div>
       </div>
 
-      <div className="w-[50%]">
+      <div className="w-[50%] hidden lg:block">
       <Image
         alt="contact image"
         src="/images/Pfxs865hdrQ0pxV1Ua1icMf4bs.avif"
