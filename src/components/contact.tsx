629956ca7fbc4208e556fe6cd27d59debfab2c75
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-06-29 16:44:38 +0300

    Refactor Contact component layout and update button styles for improved responsiveness

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,22 +1,29 @@
 import Image from "next/image"
 function Contact() {
   return (
-    <section className="flex flex-col items-center gap-10 my-52">
-      <h1 className="text-7xl font-bold">Let&apos;s Talk</h1>
-      <Image
-        alt="contact image"
-        src="/images/EmuvegPaOi1AQEF6LHkqEr04J9Y.avif"
-        width={800}
-        height={1000}
-      />
+    <section className="flex  items-center  p-20">
+      <div className="w-[50%] flex flex-col gap-5">
+      `<h1 className="text-7xl font-bold">Let&apos;s Talk</h1>
+  
         <div className="flex flex-col gap-3">
-            <button className="w-[700px] border-3 border-gray-500 py-8 rounded-2xl">
+            <button className="w-[400px] border-3 border-gray-500 py-4 rounded-2xl">
             Call Me
             </button>
-            <button className="w-[700px] border-3 border-gray-500 py-8 rounded-2xl">
+            <button className="w-[400px] border-3 border-gray-500 py-4 rounded-2xl">
             Text Me
             </button>
         </div>
+      </div>
+
+      <div className="w-[50%]">
+      <Image
+        alt="contact image"
+        src="/images/Pfxs865hdrQ0pxV1Ua1icMf4bs.avif"
+        width={800}
+        height={1000}
+      />
+      </div>
+      
     </section>
   )
 }
